9a16075cdc4b2ed9e36876232691b66aa3a0c40d
Jay Pfaffman <pfaffman@gmail.com>
2022-08-13 07:31:46 -0500

    add customfield

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-user-tag
+# about: Add tag as user_custom_field and add to topic serializer
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Jay Pfaffman
+# url: https://github.com/literatecomputing/discourse-user-tags
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :user_tag_enabled
 
 after_initialize do
+  register_editable_user_custom_field(tags, staff_only: true)
+  allow_public_user_custom_field(tags)
 end
