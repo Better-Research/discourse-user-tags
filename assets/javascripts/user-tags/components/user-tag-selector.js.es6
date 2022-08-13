edffa4c96964f6027e03ff1cd5a3c023ab3a458c
Jay Pfaffman <pfaffman@gmail.com>
2022-08-13 09:40:21 -0500

    proof of concept

---
+++ assets/javascripts/user-tags/components/user-tag-selector.js.es6
@@ -0,0 +1,12 @@
+import TagChooser from "select-kit/components/tag-chooser";
+import { makeArray } from "discourse-common/lib/helpers";
+
+export default TagChooser.extend({
+  _transformJson(context, json) {
+    window.console.log("this is context", context, json);
+    return this._super(context, json).filter((tag) => {
+      const whitelist = makeArray(context.whitelist);
+      return !whitelist.length || whitelist.indexOf(tag.id) > 1;
+    });
+  },
+});
--- plugin.rb
+++ plugin.rb
@@ -7,9 +7,17 @@
 # url: https://github.com/literatecomputing/discourse-user-tags
 # required_version: 2.7.0
 
-enabled_site_setting :user_tag_enabled
+enabled_site_setting :user_tags_enabled
 
 after_initialize do
-  register_editable_user_custom_field(tags, staff_only: true)
-  allow_public_user_custom_field(tags)
+  register_editable_user_custom_field(:tags, staff_only: false)
+  register_user_custom_field_type(:tags, 'list')
+  allow_public_user_custom_field(:tags)
+  DiscoursePluginRegistry.serialized_current_user_fields << 'tags'
+  add_to_serializer(:topic_view, :user_tags) do
+    object.topic.user.custom_fields['tags']
+  end
+  add_to_serializer(:user, :user_tags) do
+    object.custom_fields['tags']
+  end
 end
