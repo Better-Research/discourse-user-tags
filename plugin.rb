b4a718aa56e416e8fe889f5867effc75cf1ea003
Jay Pfaffman <pfaffman@gmail.com>
2022-08-15 16:41:47 -0500

    allow multiple users on same tag

--- plugin.rb
+++ plugin.rb
@@ -14,16 +14,22 @@ after_initialize do
   register_user_custom_field_type(:tags, 'list')
   allow_public_user_custom_field(:tags)
   DiscoursePluginRegistry.serialized_current_user_fields << 'tags'
-  add_to_serializer(:topic_view, :tag_owner) do
+  add_to_serializer(:topic_view, :tag_owners) do
     puts "Looking for #{object.topic.user.id}"
     puts "topic tag #{object.topic.tags.first}"
     puts "my tags #{object.topic.user.custom_fields['tags']}"
     user_tag = object.topic.user.custom_fields['tags'] ? object.topic.user.custom_fields['tags'].first : nil
     tag = object.topic.tags ? object.topic.tags.first : nil
     ucf = tag ? UserCustomField.where(value: tag.name) : nil
-    user = ucf.first ? User.find(ucf.first.user_id) : nil
+    users = []
+    ucf.each do |user|
+      name = User.find(user.user_id).username
+      puts "got the name: #{name}"
+      users.append(name)
+    end
+    puts "here are the users: #{users.first}, #{users.last}"
     puts "got the ucf: #{ucf.first}"
-    user ? user : nil
+    users
   end
   add_to_serializer(:user, :user_tags) do
     object.custom_fields['tags']
