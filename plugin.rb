57587bcfeb1a6f078ffed9e7d1f7df188da4f46d
Jay Pfaffman <pfaffman@gmail.com>
2022-08-15 14:03:16 -0500

    more proof of concept

--- plugin.rb
+++ plugin.rb
@@ -14,8 +14,16 @@ after_initialize do
   register_user_custom_field_type(:tags, 'list')
   allow_public_user_custom_field(:tags)
   DiscoursePluginRegistry.serialized_current_user_fields << 'tags'
-  add_to_serializer(:topic_view, :user_tags) do
-    object.topic.user.custom_fields['tags']
+  add_to_serializer(:topic_view, :tag_owner) do
+    puts "Looking for #{object.topic.user.id}"
+    puts "topic tag #{object.topic.tags.first}"
+    puts "my tags #{object.topic.user.custom_fields['tags']}"
+    user_tag = object.topic.user.custom_fields['tags'] ? object.topic.user.custom_fields['tags'].first : nil
+    tag = object.topic.tags ? object.topic.tags.first : nil
+    ucf = tag ? UserCustomField.where(value: tag.name) : nil
+    user = ucf.first ? User.find(ucf.first.user_id) : nil
+    puts "got the ucf: #{ucf.first}"
+    user ? user : nil
   end
   add_to_serializer(:user, :user_tags) do
     object.custom_fields['tags']
